d974bad71996b8643bf3198db8bb00993271174e
Sergey Igrunin <igrunen@bk.ru>
2021-11-19 15:31:31 +0300

    разбор package.json

---
+++ weather.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import {getArgs} from'./helpers/args.js';
+import { printHelp } from './services/log.service.js';
+
+const initCLI = ( ) => {
+	const args = getArgs(process.argv)
+
+	if (args.h) {
+		printHelp();
+	}
+	if (args.s) {
+		//Сохранить город
+	}
+	if (args.t) {
+		//Сохранить токен
+	}
+	// Вывести погоду
+};
+
+initCLI( );
