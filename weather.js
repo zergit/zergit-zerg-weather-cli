6f46de2741aa026673d5012e38eb7d8edf7aeaf4
Sergey Igrunin <igrunen@bk.ru>
2021-11-21 08:29:42 +0300

    Взаимодействие с API

--- weather.js
+++ weather.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 import {getArgs} from'./helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { getWeather } from './services/api.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+	if (!token.length) {
+		printError('Не передан token');
+		return;
+	}
+	try{
+	await saveKeyValue( TOKEN_DICTIONARY.token , token );
+	printSuccess('Токен сохранен');
+} catch (e) {
+	printError(e.message);
+}
+}
 
 const initCLI = ( ) => {
 	const args = getArgs(process.argv)
@@ -12,8 +27,10 @@ const initCLI = ( ) => {
 		//Сохранить город
 	}
 	if (args.t) {
+		return saveToken(args.t );
 		//Сохранить токен
 	}
+	getWeather('moscow')
 	// Вывести погоду
 };
 
